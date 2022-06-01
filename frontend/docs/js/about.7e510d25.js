82c936ab1398d17c9436d747ae01463ac70560b4
Vladimir Karpov <w-karpov@mail.ru>
2022-06-01 08:27:29 +0300

    init 2

---
+++ frontend/docs/js/about.7e510d25.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfrontend"]=self["webpackChunkfrontend"]||[]).push([[443],{9030:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var u=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an about page")])])}],s=t(1001),a={},l=(0,s.Z)(a,u,r,!1,null,null,null),c=l.exports}}]);
+//# sourceMappingURL=about.7e510d25.js.map
